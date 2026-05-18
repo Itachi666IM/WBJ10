204132f312c0e4d8a7a980dda829d011f0b93155
Sanj1t <rannenchi009sharingantoad2@gmail.com>
2026-05-18 19:36:32 +0530

    added basic gameplay

--- Trash Cash/Assets/Scripts/GameManager.cs
+++ Trash Cash/Assets/Scripts/GameManager.cs
@@ -1,6 +1,83 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Slider energySlider;
+    [SerializeField] private float energy = 100f;
+    [SerializeField] private GameObject timeUpScreen;
+    [SerializeField] private float energyDepreciation = 10;
 
+    [SerializeField] private Button restartButton;
+    [SerializeField] private Button shopButton;
+    [SerializeField] private Button exitButton;
+
+    [SerializeField] private GameObject pauseMenu;
+    [HideInInspector] public bool isPaused = false;
+
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button restartGameButton;
+    [SerializeField] private Button quitGameButton;
+
+    private void Awake()
+    {
+        restartButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        });
+
+        shopButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Shop");
+        });
+
+        exitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+
+        resumeButton.onClick.AddListener(() =>
+        {
+            isPaused = false;
+            pauseMenu.SetActive(false);
+        });
+        restartGameButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        });
+        quitGameButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+    private void Update()
+    {
+        if(!isPaused)
+        {
+            energy -= Time.deltaTime * energyDepreciation;
+            UpdateEnergyValue();
+        }
+        if(energy<=0)
+        {
+            isPaused = true;
+            timeUpScreen.SetActive(true);
+        }
+        if(Keyboard.current.escapeKey.wasPressedThisFrame && !isPaused)
+        {
+            isPaused= true;
+            pauseMenu.SetActive(true);
+        }
+        else if(Keyboard.current.escapeKey.wasPressedThisFrame && isPaused)
+        {
+            isPaused = false;
+            pauseMenu.SetActive(false);
+        }
+    }
+
+    public void UpdateEnergyValue()
+    {  
+        energySlider.value = energy;
+    }
 }
